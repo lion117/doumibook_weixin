180e7daf9fc5b9b003b8dbfbddfd7dad436a38c5
lion <ngixpr@qq.com>
2016-04-23 12:12:25 +0800

    1 add test project

---
+++ test/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: HeSY
+Date:  2016/4/23
+Email:	lion_117@126.com
+All Rights Reserved Licensed under the Apache License
+"""
+
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    pass
